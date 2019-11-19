285a15b101253f78d0557c40b7405474d1e4945d
Matheus Trindade <emailmartingcf1@gmail.com>
2019-11-19 18:53:59 +0000

    Create Bot-upload-pinterest.py

---
+++ Bot-upload-pinterest.py
@@ -0,0 +1,16 @@
+from c import Pinterest
+
+
+
+Bot_Matheus = Pinterest()
+
+Bot_Matheus.logar('emailmartingcf1@gmail.com','Matheuslorencia123')
+
+
+Bot_Matheus.upload_pin(
+	pasta='emagrecer',
+	titulo='Teste automático',
+	descricao='Teste Pin',
+	link_de_destino='https://www.google.com',
+	path_photo='C:\\Users\\emailmartingcf1\\Desktop\\servidor-bot\\img\\empreendedor_ozzel\\10_empreendedor_ozzel_0.jpg'
+	)
